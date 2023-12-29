9ab9e91699d6b8f1a9e201d4f1ecafdc675ffcb3
darkpc1412 <102814997+darkpc1412@users.noreply.github.com>
2023-12-29 07:11:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,7 @@ import os
 from pyromod import listen
 
 
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
